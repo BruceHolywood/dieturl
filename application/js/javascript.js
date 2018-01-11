2233e7ae8a8c8ada0c8b2fa72ee783be8b578eca
Aran <maverickjohnson@hotmail.com>
2018-01-11 08:55:54 +0000

    Updating JQ UX.

--- application/js/javascript.js
+++ application/js/javascript.js
@@ -1,1 +1,9 @@
-alert("derp");
+$( document ).ready(function() {
+	if ($(".splash-head").hasClass("splash-head-success")) {
+		$(".splash-head").hover(function(){
+       		$(".splash-subhead").text("Click to visit!");
+    	}, function(){
+        	$(".splash-subhead").text("Your new URL is above!");
+    	});
+	};
+});
