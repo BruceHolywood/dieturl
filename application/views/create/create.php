93d8a76cfc1f7e14286dee64db27744c2c7a21be
Aran <maverickjohnson@hotmail.com>
2018-01-11 13:35:31 +0000

    Updating title

--- application/views/create/create.php
+++ application/views/create/create.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="A layout example that shows off a responsive product landing page.">
-    <title>Landing Page &ndash; Layout Examples &ndash; Pure</title>
+    <title><?php echo $this->lang->line('site_title'); ?></title>
     
     <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.0/build/pure-min.css" integrity="sha384-" crossorigin="anonymous">
     <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
